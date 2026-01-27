056690be48140b259549e976c9ef8a142a478644
1kz-ma1 <kazuma012023@gmail.com>
2026-01-27 10:38:25 +0900

    update

--- intro/templatetags/custom_filters.py
+++ intro/templatetags/custom_filters.py
@@ -11,3 +11,21 @@ def image_url(image_field):
         except (AttributeError, ValueError):
             return ''
     return ''
+
+@register.filter
+def truncate_lines(text, num_lines=3):
+    """Truncate text to specified number of lines and add ellipsis"""
+    if not text:
+        return ''
+    lines = text.split('\n')
+    if len(lines) > num_lines:
+        return '\n'.join(lines[:num_lines]) + '...'
+    # Split by sentence period if fewer lines but too long
+    full_text = '\n'.join(lines[:num_lines])
+    sentences = full_text.replace('\n', ' ').split('。')
+    if len(sentences) > 1:
+        result = '。'.join(sentences[:2]) + '。'
+        if len(result) > 200:  # Limit character length
+            result = result[:200] + '...'
+        return result
+    return full_text
